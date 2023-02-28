3d5b23e772d0b42052018facb9f919b2cb5847c0
matusziak <matusziak303@gmail.com>
2023-02-28 17:01:23 +0100

    Update wording for header message

--- web/src/components/HeaderMessage.jsx
+++ web/src/components/HeaderMessage.jsx
@@ -1,7 +1,7 @@
 export default function HeaderMessage() {
   return (
     <div className="bg-green-muted text-white text-md font-bold text-center py-3 px-2">
-      This is a non-custodial wallet-less demo application developed by{" "}
+      This is a non-custodial walletless demo application developed by{" "}
       <a
         href="https://nu.fi"
         target="blank"
@@ -16,6 +16,14 @@ export default function HeaderMessage() {
         className="font-bold underline hover:opacity-80"
       >
         discord
+      </a>{" "}
+      and check our deck{" "}
+      <a
+        href="https://docsend.com/view/gujm7svb7pmxwxvr"
+        target="blank"
+        className="font-bold underline hover:opacity-80"
+      >
+        HERE
       </a>
       .
     </div>
