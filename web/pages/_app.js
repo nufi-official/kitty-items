46ff42e1fe484b4b0ad8efc8084f6e1793bbbf2e
Peter Benc <peter.benc1@gmail.com>
2023-05-17 01:50:22 +0200

    Test 0.2.0.

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -8,16 +8,16 @@ import "styles/globals.css"
 import {SWRConfig} from "swr"
 import Router from "next/router"
 import analytics from "src/global/analytics"
-import {config} from "fcl-web3auth-plugin"
+import {init} from "fcl-web3auth-plugin"
 import {useEffect} from "react"
 
 Router.events.on("routeChangeComplete", () => analytics.page())
 export default function MyApp({Component, pageProps}) {
   useEffect(() => {
-    config({
+    init({
       clientId:
         "BHpC64aYDiYE-KmGQG4oKqHNhW7aA_jBJzIWrPgSfwV-YMUBPVqKdBnyjLMt2IeG85hV3kv32DE3DzYRmSl6nqs",
-      networkType: "testnet",
+      network: "testnet",
     })
   }, [])
   return (
